b345e9b4042ec73f15e13ab9b4c36a608d4678db
chaoyue233 <beberoy@sina.com>
2024-05-14 10:50:00 +0800

    add hello world demo

---
+++ spring-01-helloworld/src/main/java/chaoyue/study/Main.java
@@ -0,0 +1,11 @@
+package chaoyue.study;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("hello.xml");
+        System.out.println(context.getBean("user"));
+    }
+}
